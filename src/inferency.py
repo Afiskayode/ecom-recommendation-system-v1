0bf77c7c957f01c828ba8dad9683b4dfadb80fc8
Afiskayode <Kayodeafis402@gmail.com>
2026-02-23 15:00:28 +0100

    inference updated

--- src/inferency.py
+++ src/inferency.py
@@ -1,1 +1,40 @@
-print('Hello word')
+import os
+import pickle
+
+data_path = '../data/'
+
+def load_model():
+
+    with open((os.path.join(data_path, 'item_user_matrix.pkl')), 'rb') as file:
+        matrix = pickle.load(file)
+
+    with open((os.path.join(data_path, 'model_knn.pkl'), 'rb')) as file:
+        model = pickle.load(file)
+
+    return model, matrix
+
+model, item_user_matrix = load_model()
+
+def get_recommendations(product_id: str, n_recommendations = 6):
+    try:
+
+        if product_id not in item_user_matrix.index:
+            return None
+         
+        distances, indices = model.kneighbors(item_user_matrix.loc[product_id, :].values.reshape(1, -1), \
+                            n_neighbors = n_recommendations)
+        
+        recommendations = []
+
+        for i in range(1, len(distances.flatten())):
+            rec_id = item_user_matrix.index[indices.flatten()[i]]
+            recommendations.append(rec_id)
+
+        return recommendations
+        
+    except Exception as e:
+        print(f"Error during inference: {e}")
+        return []
+    
+
+
